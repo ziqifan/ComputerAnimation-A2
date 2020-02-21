4d69c3c4f7f03f760f57fe32e2efa0e8c376e914
Lillian <ziqi.fan@ontariotech.net>
2020-02-21 16:04:16 -0500

    project

---
+++ Computer Animation - Particles/Assets/Script/SwitchBetweenCamera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBetweenCamera : MonoBehaviour {
+
+    public Camera firstCamera;
+    public Camera SecondCamera;
+    public Camera ThirdCamera;
+
+    private int count = 0;
+
+    private void Start()
+    {
+        firstCamera.enabled = true;
+        SecondCamera.enabled = false;
+        ThirdCamera.enabled = false;
+    }
+
+    private void Update()
+    {
+        switchCamera();
+    }
+
+    void switchCamera()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            if(count == 0)
+            {
+                firstCamera.enabled = false;
+                SecondCamera.enabled = true;
+                ThirdCamera.enabled = false;
+                count++;
+            }
+            else if(count == 1)
+            {
+                firstCamera.enabled = false;
+                SecondCamera.enabled = false;
+                ThirdCamera.enabled = true;
+                count++;
+            }
+            else
+            {
+                count = 0;
+                firstCamera.enabled = true;
+                SecondCamera.enabled = false;
+                ThirdCamera.enabled = false;
+            }
+            
+        }
+    }
+
+    
+
+}
